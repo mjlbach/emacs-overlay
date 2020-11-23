66d395ef864917a7af33ce69dc50ea5c97bb3de0
Michael Lingelbach <m.j.lbach@gmail.com>
2020-11-22 23:37:18 -0800

    Added flake-compat

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
   inputs.exwm = { url = "github:ch11ng/exwm"; flake = false; };
   inputs.xelb = { url = "github:ch11ng/xelb"; flake = false; };
   inputs.emacs-git = { url = "github:emacs-mirror/emacs"; flake = false; };
