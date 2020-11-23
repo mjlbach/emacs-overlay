62e984249972c9202378de27d5d93c446db8440c
Michael Lingelbach <m.j.lbach@gmail.com>
2020-11-22 19:39:11 -0800

    Add nixpkgs as input to derivation

--- flake.nix
+++ flake.nix
@@ -1,9 +1,34 @@
 {
   description = "Bleeding edge Emacs overlay";
 
-  outputs = { self }: {
-    # self: super: must be named final: prev: for `nix flake check` to be happy
-    overlay = final: prev:
-      import ./default.nix final prev;
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.exwm = { url = "github:ch11ng/exwm"; flake = false; };
+  inputs.xelb = { url = "github:ch11ng/xelb"; flake = false; };
+  inputs.emacs-git = { url = "github:emacs-mirror/emacs"; flake = false; };
+  inputs.emacs-unstable = { url = "github:emacs-mirror/emacs"; flake = false; };
+  inputs.emacs-pgtk = { url = "github:masm11/emacs/pgtk"; flake = false; };
+  inputs.emacs-pgtk-nativecomp = { url = "github:flatwhatson/emacs/pgtk-nativecomp"; flake = false; };
+
+  inputs.emacs-nativecomp = {
+    type = "github";
+    owner = "emacs-mirror";
+    repo = "emacs";
+    ref = "feature/native-comp";
+    flake = false;
   };
+
+  outputs = { self, flake-utils, exwm, xelb, emacs-git, emacs-unstable, emacs-pgtk, emacs-nativecomp, emacs-pgtk-nativecomp, nixpkgs }:
+    (flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      src = pkgs.callPackage ./default.nix { inherit exwm xelb emacs-git emacs-unstable emacs-pgtk emacs-nativecomp emacs-pgtk-nativecomp; };
+    in rec {
+      inherit (src) packages;
+      legacyPackages = packages;
+      defaultPackage = packages.emacsGccPgtk packages.emacsGcc;
+    }))
+    // {
+    overlay = final: prev:
+      import ./overlay.nix final prev;
+    };
 }
