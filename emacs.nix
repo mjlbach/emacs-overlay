cdd82836b58a4004c2aaefadaeca2cc60b5c920c
Michael Lingelbach <m.j.lbach@gmail.com>
2020-12-30 12:55:54 -0800

    Refactor emacs-overlay to use flakes.

---
+++ emacs.nix
@@ -0,0 +1,192 @@
+{ pkgs
+, exwm
+, xelb
+, emacs-unstable
+, emacs-git
+, emacs-nativecomp
+, emacs-pgtk
+, emacs-pgtk-nativecomp
+}:
+
+let
+  mkExDrv = emacsPackages: name: args: source:
+    emacsPackages.melpaBuild (
+      args // {
+        pname = name;
+        ename = name;
+        # version = repoMeta.version;
+        version = "28.0.5";
+        recipe = builtins.toFile "recipe" ''
+          (${name} :fetcher github
+          :repo "ch11ng/${name}")
+        '';
+
+        src = toString source;
+      }
+    );
+
+  mkGitEmacs = namePrefix: source:
+    builtins.foldl'
+      (drv: fn: fn drv)
+      pkgs.emacs
+      [
+
+        (drv: drv.override { srcRepo = true; })
+
+        (
+          drv: drv.overrideAttrs (
+            old: {
+              # name = "${namePrefix}-${repoMeta.version}";
+              name = "${namePrefix}-28.0.5.0";
+              # inherit (repoMeta) version;
+              version = "28.0.5.0";
+              src = toString source;
+
+              patches = [
+                ./patches/tramp-detect-wrapped-gvfsd.patch
+                ./patches/clean-env.patch
+              ];
+              postPatch = old.postPatch + ''
+                substituteInPlace lisp/loadup.el \
+                --replace '(emacs-repository-get-version)' '"${source.rev}"' \
+                --replace '(emacs-repository-get-branch)' '"master"'
+              '';
+              postInstall = old.postInstall + pkgs.stdenv.lib.optionalString pkgs.stdenv.isDarwin ''
+                ln -snf $out/lib/emacs/28.0.50/native-lisp $out/Applications/Emacs.app/Contents/native-lisp
+              '';
+
+            }
+          )
+        )
+
+        # Stable compat
+        (
+          drv:
+          let
+            # The nativeComp passthru attribute is used a heuristic to check if we're on 20.03 or older
+            isStable = !(pkgs.lib.hasAttr "nativeComp" (drv.passthru or { }));
+            withX = pkgs.lib.elem "--with-xft" drv.configureFlags;
+          in
+          if isStable then drv.overrideAttrs (
+            old: {
+
+              configureFlags = old.configureFlags
+                ++ pkgs.lib.optional withX "--with-cairo";
+
+              buildInputs = old.buildInputs ++ [
+                pkgs.harfbuzz.dev
+                pkgs.jansson
+              ]
+                ++ pkgs.lib.optional withX pkgs.cairo;
+
+            }
+          ) else drv
+        )
+      ];
+
+  mkPgtkEmacs = namePrefix: jsonFile: (mkGitEmacs namePrefix jsonFile).overrideAttrs (
+    old: {
+      configureFlags = (pkgs.lib.remove "--with-xft" old.configureFlags)
+        ++ pkgs.lib.singleton "--with-pgtk";
+    }
+  );
+
+  emacsGit = mkGitEmacs "emacs-git" emacs-git;
+
+  emacsGcc = (mkGitEmacs "emacs-gcc" emacs-nativecomp).override {
+    nativeComp = true;
+  };
+
+  emacsPgtk = mkPgtkEmacs "emacs-pgtk" emacs-pgtk;
+
+  emacsPgtkGcc = (mkPgtkEmacs "emacs-pgtkgcc" emacs-pgtk-nativecomp).override {
+    nativeComp = true;
+  };
+
+  emacsUnstable = (mkGitEmacs "emacs-unstable" emacs-unstable).overrideAttrs (
+    old: {
+      patches = [
+        ./patches/tramp-detect-wrapped-gvfsd-27.patch
+        ./patches/clean-env.patch
+      ];
+    }
+  );
+
+in
+{
+  inherit emacsGit emacsUnstable;
+
+  inherit emacsGcc;
+
+  inherit emacsPgtk emacsPgtkGcc;
+
+  emacsGit-nox = (
+    (
+      emacsGit.override {
+        withX = false;
+        withGTK2 = false;
+        withGTK3 = false;
+      }
+    ).overrideAttrs (
+      oa: {
+        name = "${oa.name}-nox";
+      }
+    )
+  );
+
+  emacsUnstable-nox = (
+    (
+      emacsUnstable.override {
+        withX = false;
+        withGTK2 = false;
+        withGTK3 = false;
+      }
+    ).overrideAttrs (
+      oa: {
+        name = "${oa.name}-nox";
+      }
+    )
+  );
+
+  emacsWithPackagesFromUsePackage = import ./elisp.nix { pkgs = pkgs; };
+
+  emacsWithPackagesFromPackageRequires = import ./packreq.nix { pkgs = pkgs; };
+
+  emacsPackagesFor = emacs: (
+    (pkgs.emacsPackagesFor emacs).overrideScope' (
+      eself: esuper:
+        let
+          melpaStablePackages = esuper.melpaStablePackages.override {
+            archiveJson = ./repos/melpa/recipes-archive-melpa.json;
+          };
+
+          melpaPackages = esuper.melpaPackages.override {
+            archiveJson = ./repos/melpa/recipes-archive-melpa.json;
+          };
+
+          elpaPackages = esuper.elpaPackages.override {
+            generated = ./repos/elpa/elpa-generated.nix;
+          };
+
+          orgPackages = esuper.orgPackages.override {
+            generated = ./repos/org/org-generated.nix;
+          };
+
+          epkgs = esuper.override {
+            inherit melpaStablePackages melpaPackages elpaPackages orgPackages;
+          };
+
+        in
+        epkgs // {
+          xelb = mkExDrv eself "xelb" {
+            packageRequires = [ eself.cl-generic eself.emacs ];
+          };
+
+          exwm = mkExDrv eself "exwm" {
+            packageRequires = [ eself.xelb ];
+          };
+        }
+    )
+  );
+
+}
--- flake.nix
+++ flake.nix
@@ -1,9 +1,54 @@
 {
   description = "Bleeding edge Emacs overlay";
 
-  outputs = { self }: {
-    # self: super: must be named final: prev: for `nix flake check` to be happy
-    overlay = final: prev:
-      import ./default.nix final prev;
+  inputs."pre-commit-hooks.nix" = { url = "github:cachix/pre-commit-hooks.nix"; flake = false; };
+  inputs."gitignore.nix" = { url = "github:hercules-ci/gitignore.nix"; flake = false; };
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+  inputs.exwm = { url = "github:ch11ng/exwm"; flake = false; };
+  inputs.xelb = { url = "github:ch11ng/xelb"; flake = false; };
+  inputs.emacs-git = { url = "github:emacs-mirror/emacs"; flake = false; };
+  inputs.emacs-pgtk = { url = "github:masm11/emacs/pgtk"; flake = false; };
+  inputs.emacs-pgtk-nativecomp = { url = "github:flatwhatson/emacs/pgtk-nativecomp"; flake = false; };
+
+  inputs.emacs-unstable = {
+    type = "github";
+    owner = "emacs-mirror";
+    repo = "emacs";
+    ref = "emacs-27";
+    flake = false;
   };
+
+  inputs.emacs-nativecomp = {
+    type = "github";
+    owner = "emacs-mirror";
+    repo = "emacs";
+    ref = "feature/native-comp";
+    flake = false;
+  };
+
+  outputs = { ... }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${system};
+          emacs-pkgs = with inputs; pkgs.callPackage ./emacs.nix { inherit exwm xelb emacs-git emacs-unstable emacs-pgtk emacs-nativecomp emacs-pgtk-nativecomp; };
+        in
+        {
+          packages = emacs-pkgs;
+          defaultPackage = emacs-pkgs.emacsPgtkGcc;
+          devShell = import ./shell.nix { inherit inputs pkgs; };
+        }
+      ) // {
+      overlay = _: prev:
+        let
+          pkgs = inputs.nixpkgs.legacyPackages.${prev.system};
+          emacs-pkgs = with inputs; pkgs.callPackage ./emacs.nix { inherit exwm xelb emacs-git emacs-unstable emacs-pgtk emacs-nativecomp emacs-pgtk-nativecomp; };
+        in
+        {
+          inherit (emacs-pkgs) emacsPgtkGcc emacsGcc;
+        };
+    };
 }
